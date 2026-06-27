a1fd081a49224230e2810c4ee11a1de60bc9bac6
jambi_vibes <iclg2004@gmail.com>
2026-06-27 08:58:33 -0700

    created new player object, logic to draw itself, added constants pertaining to player object. Now player is drawn very frame

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
+# imports
 import pygame
 from logger import log_state
 from constants import *
+from player import *
 
+# main code block
 def main():
+
+    # initializes pygame and creates display
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    #prints screen ratio info to console
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # Creates clock and delta time var
     clock = pygame.time.Clock()
     dt = 0.0
+
+    # Objects
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    
+
+    # main event handler for game loop
     while True:
         log_state()
         for event in pygame.event.get():
@@ -19,6 +34,7 @@ def main():
             
         dt = clock.tick(60) / 1000
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
 if __name__ == "__main__":
---
+++ player.py
@@ -0,0 +1,23 @@
+# imports
+from constants import *
+from circleshape import *
+
+# Creates Player class and inherits from CircleShape for hitbox
+class Player(CircleShape):
+    def __init__(self, x, y):
+
+        super().__init__(x, y, PLAYER_RADIUS)
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
+
+    rotation = 0
+
+    # Draws player sprite as triangle
+    def triangle(self) -> list[pygame.Vector2]:
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
