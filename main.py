9119a4dd442e41debfee70721f3b5d056f097f2e
jambi_vibes <iclg2004@gmail.com>
2026-06-27 12:26:46 -0700

    Added Asteroids and created Groups
    Created groups and containers for updatable obj, drawable obj, and asteroids. Created logic to draw asteroids randomly, and hooked to main game loop

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pygame
 from logger import log_state
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import AsteroidField
 
 # main code block
 def main():
@@ -20,10 +22,20 @@ def main():
     clock = pygame.time.Clock()
     dt = 0.0
 
+    # Groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+
+    # Containers
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     # Objects
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
-    
+    asteroidfield = AsteroidField()
 
     # main event handler for game loop
     while True:
@@ -34,8 +46,9 @@ def main():
             
         dt = clock.tick(60) / 1000
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
 
 if __name__ == "__main__":
